c19860267fae572825cd96e2162d539be2ddf0e5
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-07-13 07:56:23 -0600

    version Final

--- Proyecto/routes/web.php
+++ Proyecto/routes/web.php
@@ -27,6 +27,7 @@ Route::get('/', function () {
 
 Route::get('/clientes',[ClienteController::class,'index'])->name("clientes");
 Route::get("/registroclientes",[RegistroCliente::class,'index'])->name('registroClientes');
+Route::post("/regsitroC",[RegistroClientes::class,'store']);
 
 //Ruta para mostrar la vista de login
 Route::get('/login',[LoginController::class,'index'])->name("login");
