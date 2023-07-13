79c38e0e1dc43cc092211749f3644675e217cd00
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-07-13 08:23:55 -0600

    Clientes Final

--- Proyecto/app/Http/Controllers/LoginController.php
+++ Proyecto/app/Http/Controllers/LoginController.php
@@ -11,7 +11,21 @@ class LoginController extends Controller
         return view('loginInicio');
     }
 
-    public function store(){
-        
+    //Metodo de validacion de formulario de laogin
+    public function store(Request $request){
+        //Depurar para ver su funcionalidad
+        $this->validate($request,[
+        'email' => 'required|email',
+        'password' => 'required'
+        ]);
+
+        //Valacion de credenciales correctas
+        if(!auth()->attempt($request->only('email','password'),$request->remember)){
+            //Utilizar directiva "with" para llenar los valores de la sesion
+            return back()->with('mensaje','credenciales incorrectas');
+        }
+
+        //Credenciales correctas
+        return redirect()->route('loginInicio');
     }
 }
