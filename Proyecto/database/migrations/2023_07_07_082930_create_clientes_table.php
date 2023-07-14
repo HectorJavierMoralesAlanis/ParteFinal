323ee91d097ba05ea88d9a21cfbab5f7efbd9eee
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-07-14 01:19:52 -0600

    version Final

--- Proyecto/database/migrations/2023_07_07_082930_create_clientes_table.php
+++ Proyecto/database/migrations/2023_07_07_082930_create_clientes_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->string('emailC')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('passwordC');
-            $table->string('repasswordC');
             $table->string('telefonoC');
             $table->string('compNombreC');
             $table->rememberToken();
