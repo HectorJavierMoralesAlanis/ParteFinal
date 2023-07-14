2f465b8af46e60ae71d79c0f35163c26d74a0b11
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-07-14 07:54:48 -0600

    version Final

--- Proyecto/app/Http/Controllers/DashboardController.php
+++ Proyecto/app/Http/Controllers/DashboardController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     //
     public function index(){
-        return view('dashboardAdmin');
+        $colaboradores = DB::table('colaboradores');
+        return view('dashboardAdmin')->with('colaboradores',$colaboradores);
     }
 }
