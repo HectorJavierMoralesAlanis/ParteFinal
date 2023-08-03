3c7bdba1b2052106d4df43e4b37a10cf1e979ed1
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-08-03 04:04:31 -0600

    version Final

--- Proyecto/resources/views/layouts/app.blade.php
+++ Proyecto/resources/views/layouts/app.blade.php
@@ -32,7 +32,6 @@
         </header>
         
         <div class="flex flex-row">
-
             @if(auth()->user()->id==1) 
             <div class="fixed top-20 left-0 z-40 h-screen text-center transistion-transform-translate-x-full sm:translate-x-0 bg-neutral-900 shadow p-4 position: sticky; basis-1/4">
                 <div class="h-full px-3 py-4 overflow-y-auto bg-neutral-900 dark:bg-neutral-800">
