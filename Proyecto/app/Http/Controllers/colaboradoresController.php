8ef0d2bf2e96a04be5247a11aabe25106b68ad4e
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-08-03 00:37:14 -0600

    version Final

--- Proyecto/app/Http/Controllers/colaboradoresController.php
+++ Proyecto/app/Http/Controllers/colaboradoresController.php
@@ -10,7 +10,7 @@ class colaboradoresController extends Controller
     //
     public function index(){
         $colaborador = DB::table('colaboradores')->get();
-        return view('colaboradores')->with('colaborador',$colaborador);
+        return view('colaboradores')->with('colaboradores',$colaborador);
     }
     /*
     public function create(){
