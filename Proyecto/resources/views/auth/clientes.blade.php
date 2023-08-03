781f636c6c42fbcb3ec95e91641824c7cc279325
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-08-03 02:57:06 -0600

    version Final

--- Proyecto/resources/views/auth/clientes.blade.php
+++ Proyecto/resources/views/auth/clientes.blade.php
@@ -14,31 +14,31 @@
             </div>
         </header>
 
-        
-        <div class="bg-slate-400 shadow-2xl shadow-slate-400 rounded-md container mx-auto px-10">
-            <table class="bg-slate-200 shadow-xl shadow-slate-200 font-bold text-xl mb-10 rounded-md container mx-auto px-40">
-                <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>Nombre</th>
-                        <th>Telefono</th>
-                        <th>Correo</th>
-                        <th>Compañia</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($clientes as $cliente)
-                    <tr>
-                        <td>{{$cliente->id}}</td>
-                        <td>{{$cliente->nombreC}}</td>
-                        <td>{{$cliente->telefonoC}}</td>
-                        <td>{{$cliente->emailC}}</td>
-                        <td>{{$cliente->compNombreC}}</td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-        
+        <section class="w-[90%] max-w-md mx-auto">
+            <div class="bg-slate-400 shadow-2xl shadow-slate-400 rounded-md container mx-auto px-10">
+                <table class="bg-slate-200 shadow-xl shadow-slate-200 font-bold text-xl mb-10 rounded-md container mx-auto px-40 p-10">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Nombre</th>
+                            <th>Telefono</th>
+                            <th>Correo</th>
+                            <th>Compañia</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($clientes as $cliente)
+                        <tr>
+                            <td>{{$cliente->id}}</td>
+                            <td>{{$cliente->nombreC}}</td>
+                            <td>{{$cliente->telefonoC}}</td>
+                            <td>{{$cliente->emailC}}</td>
+                            <td>{{$cliente->compNombreC}}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </section>
     </div>
 @endsection
