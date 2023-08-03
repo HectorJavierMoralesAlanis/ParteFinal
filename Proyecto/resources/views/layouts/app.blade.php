83d1a8bb1c7a6445b8ff27c8875b8d0633291e7e
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-08-03 05:12:04 -0600

    version Final

--- Proyecto/resources/views/layouts/app.blade.php
+++ Proyecto/resources/views/layouts/app.blade.php
@@ -32,6 +32,7 @@
             </div>
         </header>
         <div class="flex flex-column h-full">
+            <div class="flex-initial w-64">
             @if(auth()->user()->id==1) 
             <div class="flex-initial w-64 h-full">
                 <div class="h-full px-3 py-4 overflow-y-auto bg-neutral-900 dark:bg-neutral-800">
@@ -129,6 +130,7 @@
                 </div>
             </aside>
             @endif
+            </div>
             <div class="flex-initial w-full">
                 <!-- Contenido de las vistas -->
                 <div class="p-4 border-2 border-gray-200 rounded-lg dark:border-gray-700">
