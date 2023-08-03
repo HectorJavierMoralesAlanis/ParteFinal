89fa5b2566feb5a9f4363739c7da0457942468f0
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-08-03 04:31:01 -0600

    version Final

--- Proyecto/resources/views/layouts/app.blade.php
+++ Proyecto/resources/views/layouts/app.blade.php
@@ -130,12 +130,10 @@
             @endif
             <div class="shrink w-full h-14">
                 <!-- Contenido de las vistas -->
-                <main>
                     <h2 class="font-black text-center text-3xl mb-10 text-black">
                         @yield('titulo')
                     </h2>
                     @yield('contenido')
-                </main>
                 @endauth
                 <footer class="text-center p-5 text-gray-500 font-bold">
                     Derechos reservados {{now()->year}}
