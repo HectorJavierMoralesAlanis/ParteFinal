489069050daf42ba8916f9240d8ea57a491f2d76
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-08-03 05:15:40 -0600

    version Final

--- Proyecto/resources/views/layouts/app.blade.php
+++ Proyecto/resources/views/layouts/app.blade.php
@@ -131,7 +131,7 @@
             </aside>
             @endif
             </div>
-            <div class="flex-initial w-full">
+            <div class="flex-initial">
                 <!-- Contenido de las vistas -->
                 <div class="p-4 border-2 border-gray-200 rounded-lg dark:border-gray-700">
                 <main class="container mx-auto MT-10">
