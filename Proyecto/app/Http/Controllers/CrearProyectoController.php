a3c38727814b42d16588fead8db1eb1d83a1ee84
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-08-03 02:27:17 -0600

    version Final

--- Proyecto/app/Http/Controllers/CrearProyectoController.php
+++ Proyecto/app/Http/Controllers/CrearProyectoController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 
 
-use Illuminate\Http\Request;
+use App\Models\proyectos;
 use Illuminate\Support\Str;
-use App\Models\colaboradores;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Hash;
 
-class CrearColaboradorController extends Controller
+class CrearProyectosController extends Controller
 {
     //
     public function index() {
@@ -21,33 +22,30 @@ class CrearColaboradorController extends Controller
 
     public function store(Request $request){
         $this->validate($request,[
-            'nombrecolaborador' => "required|max:255",
-            'apellidocolaborador' => "required|max:255",
-            'usernamecolaborador'=> "required",
-            'passwordcolaborador'=> "required",
-            'joindatecolaborador' => "required",
-            'telefonocolaborador' => "required",
-            'companiacolaborador'=> "required",
-            'departamentocolaborador'=> "required",
-            'designacioncolaborador'=> "required"
+            'nombreproyecto' => "required|max:255",
+            'nombrecliente' => "required|max:255",
+            'descripcionproyecto'=> "required",
+            'fechaempezado'=> "required",
+            'fechaterminado' => "required",
+            'presupuesto' => "required",
+            'prioridad'=> "required",
+            'liderproyecto'=> "required",
+            'nombreteam'=> "required"
 
         ]);
-    $colaboradores = new colaboradores;
-    $colaboradores->nombrecolaborador = $request->nombrecolaborador;
-    $colaboradores->apellidocolaborador = $request->apellidocolaborador;
-    $colaboradores->usernamecolaborador = Str::slug($request->usernamecolaborador);
-    $colaboradores->passwordcolaborador = Hash::make($request->passwordcolaborador);
-    $colaboradores->joindatecolaborador = $request->joindatecolaborador;
-    $colaboradores->telefonocolaborador = $request->telefonocolaborador;
-    $colaboradores->companiacolaborador = $request->companiacolaborador;
-    $colaboradores->departamentocolaborador = $request->departamentocolaborador;
-    $colaboradores->designacioncolaborador = $request->designacioncolaborador;
-    $colaboradores->save();
-    
-    
-    //dd('creaste el proyecto');
-    
-    return redirect()->route('colaboradores');
+        proyectos::create([
+            'nombreproyectos' => $request->nombreproyectos,
+            'nombreclientes' => $request->nombrecliente,
+            'descripcionproyecto' => $request->descripcionproyecto,
+            'fechaempezado' => $request->fechaempezado,
+            'fechaterminado' => $request->fechatermiando,
+            'presupuesto' => $request->presupuesto,
+            'prioridad' => $request->prioridad,
+            'liderproyecto' => $request->liderproyecto,
+            'nombreteam' => $request->nombreteam,
+        ]);
+        $proyectos = DB::table('proyectos');
+        return view('proyectos')->with('proyectos',$proyectos);
     
     }
     
--- Proyecto/app/Http/Controllers/colaboradoresController.php
+++ Proyecto/app/Http/Controllers/colaboradoresController.php
@@ -12,23 +12,5 @@ class colaboradoresController extends Controller
         $colaborador = DB::table('colaboradores')->get();
         return view('colaboradores')->with('colaboradores',$colaborador);
     }
-    
-    /*
-    public function create(){
-        return view ('colaboradores');
-    }
-    public function show(colaboradores $colaboradores){
-        return view('colaboradores',[
-            'colaboradores' => $colaboradores::all()
-            
-        ]);
-        //dd('ola');
-    }
-    public function showColab(colaboradores $colaboradores){
-        return view('crearcolaborador',[
-            'colaboradores' => $colaboradores::all()
-            
-        ]);
-        //dd('ola');
-    }*/
+
 }
