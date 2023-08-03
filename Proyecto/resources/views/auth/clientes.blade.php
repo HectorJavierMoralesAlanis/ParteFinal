e2baadeca080163f42b673958fdfc006c80a825f
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-08-03 03:02:50 -0600

    version Final

--- Proyecto/resources/views/auth/clientes.blade.php
+++ Proyecto/resources/views/auth/clientes.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('contenido')
-    <div class="w-[90%] max-w-md mx-auto">
+    <div>
         <header>
             <div class="container mx-auto flex justify-between items-center">
                 <nav class="md:flex md:items-center md:justify-between">
@@ -13,7 +13,7 @@
                 </nav>
             </div>
         </header>
-        
+
         <section class="w-[90%] max-w-md mx-auto">
             <div class="bg-slate-400 shadow-2xl shadow-slate-400 rounded-md container mx-auto px-10">
                 <table class="bg-slate-200 shadow-xl shadow-slate-200 font-bold text-xl mb-10 rounded-md container mx-auto px-40 p-10">
