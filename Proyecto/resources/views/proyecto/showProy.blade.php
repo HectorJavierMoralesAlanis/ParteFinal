9b6bcade89c10f31a7a26745dee9c582759dda06
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-08-15 03:04:08 -0600

    version Final

--- Proyecto/resources/views/proyecto/showProy.blade.php
+++ Proyecto/resources/views/proyecto/showProy.blade.php
@@ -10,14 +10,16 @@
             <div class="w-8/12 lg:w-6/12 px-5">
                 <img src="{{ asset('img/colaboradores/proyectos.avif')}}" alt="imagen de perfil" width="400" height="400"> 
             </div>
-            
-            <h4 class="user-name m-t-10 mb-0 text-ellipsis font-bold text-center">
-                    Nombre del proyecto: {{ $proyectos[$id] }}  
-            </h4>
+            @foreach($proyectos as $proyecto)
+                @if($proyecto->id == $id)
+                <h4 class="user-name m-t-10 mb-0 text-ellipsis font-bold text-center">
+                        Nombre del proyecto: {{ $proyecto->nombreproyecto }}  
+                </h4>
 
-            <div class="small text-muted text-center">
-                    Nombre del cliente: 
-            </div>
+                <div class="small text-muted text-center">
+                        Nombre del cliente: {{ $proyecto->clientes}}
+                </div>
+            @endforeach
         </div>        
     </div>
 @endsection
