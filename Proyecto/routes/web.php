a6bff8aacb58caa17b64fd5c76939ea8766ad13c
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-08-15 04:00:26 -0600

    version Final

--- Proyecto/routes/web.php
+++ Proyecto/routes/web.php
@@ -84,6 +84,7 @@ Route::post('/crearproyecto',[CrearProyectoController::class, 'store']);
 //Ruta para almacenar los colaboradores
 //Route::post('/crearcolaborador',[CrearColaboradorController::class, 'store']);
 
+
 Route::get('/colaboradores', [colaboradoresController::class, 'index'])->name('colaboradores');
 Route::get('/colaboradores', [colaboradoresController::class, 'show'])->name('colaboradores');
 //Route::get('/{colaborador:usernamecolaborador}',[colaboradoresController::class, 'showColab'])->name('colaboradores.show');
