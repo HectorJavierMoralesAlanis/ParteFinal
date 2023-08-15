3f082a256aa7c86648467e8eefb1d4b5babd673b
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-08-15 02:48:29 -0600

    version Final

--- Proyecto/app/Http/Controllers/proyectosController.php
+++ Proyecto/app/Http/Controllers/proyectosController.php
@@ -20,7 +20,7 @@ class proyectosController extends Controller
     }
     public function showProy(proyectos $proyectos,$proyecto){
         $proyectoN = $proyecto;
-        dd($proyectos::all());
+        dd($proyectos->getattributes());
         foreach($proyectos as $pro):
             $id=$pro;
             if($id == $proyecto):
