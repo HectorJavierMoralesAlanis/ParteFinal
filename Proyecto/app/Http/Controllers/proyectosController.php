d4690c63db398225abc38587b4787c89b52d48b0
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-08-15 02:32:39 -0600

    version Final

--- Proyecto/app/Http/Controllers/proyectosController.php
+++ Proyecto/app/Http/Controllers/proyectosController.php
@@ -18,6 +18,7 @@ class proyectosController extends Controller
             'proyectos' => $proyectos::all()
         ]);
     }
+    
     public function showProy(proyectos $proyectos,$proyecto){
         $proyectoN = $proyecto;
         foreach($proyectos as $pro):
