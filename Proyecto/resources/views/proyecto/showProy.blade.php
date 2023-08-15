e06e919327e2749493879f6dbf3aa8d0d022bb30
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-08-15 03:04:25 -0600

    version Final

--- Proyecto/resources/views/proyecto/showProy.blade.php
+++ Proyecto/resources/views/proyecto/showProy.blade.php
@@ -12,13 +12,12 @@
             </div>
             @foreach($proyectos as $proyecto)
                 @if($proyecto->id == $id)
-                <h4 class="user-name m-t-10 mb-0 text-ellipsis font-bold text-center">
-                        Nombre del proyecto: {{ $proyecto->nombreproyecto }}  
-                </h4>
-
-                <div class="small text-muted text-center">
-                        Nombre del cliente: {{ $proyecto->clientes}}
-                </div>
+                    <h4 class="user-name m-t-10 mb-0 text-ellipsis font-bold text-center">
+                            Nombre del proyecto: {{ $proyecto->nombreproyecto }}  
+                    </h4>
+                    <div class="small text-muted text-center">
+                            Nombre del cliente: {{ $proyecto->clientes}}
+                    </div>
             @endforeach
         </div>        
     </div>
