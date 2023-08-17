3ca4683e13daa6bfd78d1c77b9954326bb067c8d
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-08-17 04:52:14 -0600

    version Final

--- Proyecto/app/Http/Controllers/ClienteController.php
+++ Proyecto/app/Http/Controllers/ClienteController.php
@@ -29,7 +29,6 @@ class ClienteController extends Controller
         dd($id);
 
         #return view('auth.clienteAct',compact($clientes));
-        
     }
     public function update(Request $request,Cliente $cliente){
         $cliente->nombreC=$request->nombreC;
