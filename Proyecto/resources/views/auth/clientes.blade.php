5cba5b788abdbaa25ad10039ad30790479961294
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-08-17 03:35:46 -0600

    version Final

--- Proyecto/resources/views/auth/clientes.blade.php
+++ Proyecto/resources/views/auth/clientes.blade.php
@@ -39,7 +39,7 @@
                         <td>{{$cliente->emailC}}</td>
                         <td>{{$cliente->compNombreC}}</td>
                         <td><a class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">Editar</a></td>
-                        <td><a href="{{route('cliente.destroy',['cliente'=>$cliente])}}" class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg" type="submit">
+                        <td><a href="{{route('cliente.destroy',['cliente'=>$cliente->id])}}" class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg" type="submit">
                                 Eliminar 
                         </a></td>
                     </tr>
