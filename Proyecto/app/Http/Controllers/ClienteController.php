c7a3a325313b18edd08c4aff6b10e1b117444755
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-08-17 02:58:37 -0600

    version Final

--- Proyecto/app/Http/Controllers/ClienteController.php
+++ Proyecto/app/Http/Controllers/ClienteController.php
@@ -14,9 +14,10 @@ class ClienteController extends Controller
         $clientes = DB::table('clientes')->get();
         return view('auth.clientes')->with('clientes',$clientes);
     }
-    
+
     public function destroy(Cliente $cliente){
         $cliente->delete();   
         return redirect()->route('clientes');
     }
+    
 }
