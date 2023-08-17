e02b68b69d2b3fd738cbd751a2758db16fb4fc2a
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-08-17 04:02:10 -0600

    version Final

--- Proyecto/resources/views/auth/clientes.blade.php
+++ Proyecto/resources/views/auth/clientes.blade.php
@@ -34,8 +34,6 @@
                         <td>{{$cliente->compNombreC}}</td>
                         <td><a class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">Editar</a></td>
                         <td><form href="{{route('cliente.destroy',['cliente'=>$cliente->id])}}"> 
-                            @csrf
-                            @method('delete')
                             <button class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg" type="submit">
                                 Eliminar 
                             </button>
