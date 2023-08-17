a4c25713fe99a26020ea506653768e934cb90f46
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-08-17 03:53:51 -0600

    version Final

--- Proyecto/resources/views/auth/clientes.blade.php
+++ Proyecto/resources/views/auth/clientes.blade.php
@@ -24,6 +24,7 @@
                         <th>Compañia</th>
                     </tr>
                 </thead>
+                
                 <tbody>
                     @foreach($clientes as $cliente)
                     <tr>
