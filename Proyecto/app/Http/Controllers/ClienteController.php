aabebc726f3bafdfa7143588e93de18223885850
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-08-17 02:58:56 -0600

    version Final

--- Proyecto/app/Http/Controllers/ClienteController.php
+++ Proyecto/app/Http/Controllers/ClienteController.php
@@ -9,9 +9,8 @@ use Illuminate\Support\Facades\DB;
 class ClienteController extends Controller
 {
     //
-    
+
     public function index(){
-        
         $clientes = DB::table('clientes')->get();
         return view('auth.clientes')->with('clientes',$clientes);
     }
