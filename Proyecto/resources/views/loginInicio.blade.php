a67e52f9f676e2452c201991f10870a476899150
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-08-17 06:14:01 -0600

    version Final

--- Proyecto/resources/views/loginInicio.blade.php
+++ Proyecto/resources/views/loginInicio.blade.php
@@ -58,11 +58,10 @@
             />
         </form>
     </div>
-
+</div>
     <footer>
         <h4>Integrantes</h4>
         <p>Alex Guillermo Castillo Nava</p>
         <p>HectorJavierMorales Alanis</p>
     </footer>
-</div>
 @endsection
